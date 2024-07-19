712a1604d746374426ca568fc9ddac7d8253aad2
Sanju-Shrestha <sanjushresthanp@gmail.com>
2024-07-19 21:20:11 +0545

    data_collection.py updated using selenium

--- transfer-market-tracker/README.md
+++ transfer-market-tracker/README.md
@@ -1,1 +1,4 @@
 Transfer Market Tracker
+
+src/data_collection.py 
+Tried using BeautifulSoup but unable to collect data as desired. Changed the approach by using Selenium since the website is dynamic. The second commit is for single league data extraction only.
--- transfer-market-tracker/src/data_collection.py
+++ transfer-market-tracker/src/data_collection.py
@@ -1,104 +1,130 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 import pandas as pd
-import requests
-from bs4 import BeautifulSoup, Comment
-from sqlalchemy import create_engine
 
-'''
-For Web scrapping, i'll be using fbref website
-https://fbref.com
-'''  
+driver = webdriver.Chrome()
 
-url = 'https://fbref.com/en/comps/9/stats/Premier-League-Stats'
+driver.get("Insert the website link here")   # udpate required
 
-# Sending a get request to the website
-response = requests.get(url)
-soup = BeautifulSoup(response.content,'html.parser')
+players = driver.find_elements(By.XPATH,"#Update the XPATH here#/td[1]/a")
 
-# Starting the data extraction
-players = [] # List to store player data
+nations = driver.find_elements(By.XPATH,"#Update the XPATH here#/td[2]/a/span/span")
 
-# Within the website we need find the table with player stats
-# Finding the comment containing the table with player stats
-comments = soup.find_all(string=lambda text: isinstance(text, Comment))
-for comment in comments:
-    if 'table_container' in comment:
-        table_soup = BeautifulSoup(comment, 'html.parser')
-        table = table_soup.find('table', {'id': 'stats_standard'})
-        break
+positions = driver.find_elements(By.XPATH,"#Update the XPATH here#/td[3]")
+
+squads = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[4]/a")
+
+ages = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[5]")
+
+borns = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[6]")
+
+MPs = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[7]")
+
+starts = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[8]")
+
+minutes = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[9]")
+
+nineties = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[10]")
+
+Glss = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[11]")
+
+Asts = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[12]")
+
+GAs = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[13]")
+
+G_PKs = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[14]")
+
+PKs = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[15]")
+
+PKatts = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[16]")
+
+CrdYs = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[17]")
+
+CrdRs = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[18]")
+
+xGs = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[19]")
+
+npxGs = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[20]")
+
+xAGs = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[21]")
+
+npxG_xAGs = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[22]")
+
+PrgCs = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[23]")
+
+PrgPs = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[24]")
+
+PrgRs = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[25]")
+
+Gls_per90s = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[26]")
+
+Ast_per90s = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[27]")
+
+GA_per90s = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[28]")
+
+G_PK_per90s = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[29]")
+
+GA_PK_per90s = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[30]")
+
+xG_per90s = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[31]")
+
+xAG_per90s = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[32]")
+
+xG_xAG_per90s = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[33]")
+
+npxG_per90s = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[34]")
+
+npxG_xAG_per90s = driver.find_elements(By.XPATH, "#Update the XPATH here#/td[35]")
 
 
-# Checkinf if the table was found
-  
-if table is not None:
-    # Traversing through the table with player stats
-    tbody = table.find('tbody')
-    if tbody:
-        for row in tbody.find_all('tr'):
-            th_player = row.find('th', {'data-stat': 'player'})
-            if th_player:
-                name = th_player.text.strip()
-                position = row.find('td', {'data-stat': 'position'})
-                if position:
-                    position = position.text.strip()
-                else:
-                    position = 'Unknown'
+# for postion in positions:
+#     print(postion.text)
 
-                age = row.find('td', {'data-stat': 'age'})
-                if age:
-                    age = age.text.strip()
-                else:
-                    age = 'Unknown'
 
-                club = row.find('td', {'data-stat': 'team'})
-                if club:
-                    club = club.text.strip()
-                else:
-                    club = 'Unknown'
+result = []
 
-                appearances = row.find('td', {'data-stat': 'games'})
-                if appearances:
-                    appearances = appearances.text.strip()
-                else:
-                    appearances = '0'
+for i in range(len(players)):
+    temporary_data = {
+        'Player': players[i].text,
+        'Nation': nations[i].text,
+        'Position': positions[i].text,
+        'Squad': squads[i].text,
+        'Age': ages[i].text,
+        'Born': borns[i].text,
+        'MP': MPs[i].text,
+        'Starts': starts[i].text,
+        'Min': minutes[i].text,
+        '90s': nineties[i].text,
+        'Gls': Glss[i].text,
+        'Ast': Asts[i].text,
+        'G+A': GAs[i].text,
+        'G-PK': G_PKs[i].text,
+        'PK': PKs[i].text,
+        'PKatt': PKatts[i].text,
+        'CrdY': CrdYs[i].text,
+        'CrdR': CrdRs[i].text,
+        'xG': xGs[i].text,
+        'npxG': npxGs[i].text,
+        'xAG': xAGs[i].text,
+        'npxG+xAG': npxG_xAGs[i].text,
+        'PrgC': PrgCs[i].text,
+        'PrgP': PrgPs[i].text,
+        'PrgR': PrgRs[i].text,
+        'Gls_per90': Gls_per90s[i].text,
+        'Ast_per90': Ast_per90s[i].text,
+        'G+A_per90': GA_per90s[i].text,
+        'G-PK_per90': G_PK_per90s[i].text,
+        'G+A-PK_per90': GA_PK_per90s[i].text,
+        'xG_per90': xG_per90s[i].text,
+        'xAG_per90': xAG_per90s[i].text,
+        'xG+xAG_per90': xG_xAG_per90s[i].text,
+        'npxG_per90': npxG_per90s[i].text,
+        'npxG+xAG_per90': npxG_xAG_per90s[i].text
+        }
+    
+    result.append(temporary_data)
 
-                goals = row.find('td', {'data-stat': 'goals'})
-                if goals:
-                    goals = goals.text.strip()
-                else:
-                    goals = '0'
+df_data = pd.DataFrame(result)
+df_data.to_csv('data/raw/Premier-League-Data.csv',index=False)
 
-                assists = row.find('td', {'data-stat': 'assists'})
-                if assists:
-                    assists = assists.text.strip()
-                else:
-                    assists = '0'
-
-                players.append({
-                    'Name': name,
-                    'Age': age,
-                    'Position': position,
-                    'Club': club,
-                    'Appearances': appearances,
-                    'Goals': goals,
-                    'Assists': assists
-                })
-            else:
-                print("Player name not found for a row. Skipping.")
-    else:
-        print("Table body not found.")
-        
-else:
-    print("Table not found.")
-
-# Converting the list of players into a DataFrame
-df = pd.DataFrame(players)
-
-# Save data in CSV format
-csv_path = 'data/raw/fbref_player_stats.csv'
-df.to_csv(csv_path, index=False)
-
-# Creating a connection to an SQLite database
-engine = create_engine('sqlite:///data/raw/players_data.db')
-
-# Save DataFrame to SQL database
-df.to_sql('player_stats', engine, if_exists='replace', index=False)
